138fbd332126243f9ba515be8f56312c69016425
Eric Hurst <eh@Gideon.local>
2009-07-30 18:44:00 -0500

    Added the ability to search multiple users

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -5,18 +5,25 @@ class ColorsController < ApplicationController
   end
   
   def create
-    username = params[:username].gsub('@','').to_s
+    usernames = params[:usernames].gsub(/[@,]/,'').split(/[\s]/)
     
-    if username != ""
-      @user = Twitter.user(username)
-      render :update do |page|
-        page << "if($('fat_finger')){"
-          page.visual_effect :fade, "fat_finger", :duration => "0.3"
-        page << "}"
-        page.replace_html "results", :partial => "results"
-        page.visual_effect :highlight, "results", :startcolor => "#6CC", :endcolor => "#FFF"
-        page[:entry_form].reset
-      end
+    if usernames != ""
+        render :update do |page|
+          page << "if($$('result')){"
+            page.replace_html "results"
+          page << "}"
+          page.insert_html :top, "results", "<h3>Those are some nice colors!</h3>"
+          
+          usernames.each do |username|
+            @user = Twitter.user(username)
+            page << "if($('fat_finger')){"
+              page.visual_effect :fade, "fat_finger", :duration => "0.3"
+            page << "}"
+            page.insert_html :bottom, "results", :partial => "result"
+            page.visual_effect :highlight, "results", :startcolor => "#6CC", :endcolor => "#FFF"
+            page[:entry_form].reset
+          end
+        end
     else
       render :update do |page|
         page.insert_html :top, "header", "<div id='fat_finger'><h2>FAT FINGER ALERT: Hexminr requires a Twitter username</h2></div>"
