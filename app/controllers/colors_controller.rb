90383dccbc5185222b0fd36acd567d356c52da2a
Eric Hurst <eh@Gideon.local>
2009-08-17 09:25:39 -0500

    Fixed fat-finger error, styled with border-radius

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -7,23 +7,23 @@ class ColorsController < ApplicationController
   def create
     usernames = params[:usernames].gsub(/[@,]/,'').split(/[\s]/)
     
-    if usernames != ""
-        render :update do |page|
-          page << "if($$('result')){"
-            page.replace_html "results"
+    unless usernames.empty?
+      render :update do |page|
+        page << "if($$('result')){"
+          page.replace_html "results"
+        page << "}"
+        page.insert_html :top, "results", "<h3>Those are some nice colors!</h3>"
+      
+        usernames.each do |username|
+          @user = Twitter.user(username)
+          page << "if($('fat_finger')){"
+            page.visual_effect :fade, "fat_finger", :duration => "0.3"
           page << "}"
-          page.insert_html :top, "results", "<h3>Those are some nice colors!</h3>"
-          
-          usernames.each do |username|
-            @user = Twitter.user(username)
-            page << "if($('fat_finger')){"
-              page.visual_effect :fade, "fat_finger", :duration => "0.3"
-            page << "}"
-            page.insert_html :bottom, "results", :partial => "result"
-            page.visual_effect :highlight, "results", :startcolor => "#6CC", :endcolor => "#FFF"
-            page[:entry_form].reset
-          end
+          page.insert_html :bottom, "results", :partial => "result"
+          page.visual_effect :highlight, "results", :startcolor => "#6CC", :endcolor => "#FFF"
+          page[:entry_form].reset
         end
+      end
     else
       render :update do |page|
         page.insert_html :top, "header", "<div id='fat_finger'><h2>FAT FINGER ALERT: Hexminr requires a Twitter username</h2></div>"
